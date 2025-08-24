6b86d3cf076181d14f47c9cb9038a6e18c2cf3de
Julien Kmec <me@julien.dev>
2025-08-23 22:42:14 -0700

    fix

--- pydantic_encryption/models/base.py
+++ pydantic_encryption/models/base.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from super_model import SuperModel
+
 from . import SecureModel
 
 __all__ = ["BaseModel"]
@@ -6,3 +9,8 @@ __all__ = ["BaseModel"]
 
 class BaseModel(SuperModel, SecureModel):
     """Base model for encryptable models."""
+
+    def model_post_init(self, context: Any, /) -> None:
+        self.default_post_init()
+
+        super().model_post_init(context)
